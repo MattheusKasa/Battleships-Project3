d89d6e2b20870b513f2cdd5a32cbd75a8d3ad37b
MattheusKasa <mattheus.kasa@hotmail.se>
2022-06-28 23:58:47 +0000

    Finalized project

--- run.py
+++ run.py
@@ -1,3 +1,71 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Main board for displaying ships
+CONCEALED_BOARD = [[" "] * 6 for x in range(6)]
+# Displays if its a hit or a miss
+USER_BOARD = [[" "] * 6 for i in range(6)]
+
+
+def print_board(board):
+    print("  A B C D E F  ")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, }
+
+
+# The computer creates 5 ships within the board
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 5), randint(0, 5)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = get_ship_location()
+        board[ship_row][ship_column] = "X"
+
+
+def get_ship_location():
+    row = input("Enter the row of the battleship: ").upper()
+    while row not in "123456":
+        print('Not a valid choice, please select a valid row')
+        row = input("Enter the row of the battleship: ").upper()
+    column = input("Enter the column of the battleship: ").upper()
+    while column not in "ABCDEF":
+        print('Not a valid choice, please select a valid column')
+        column = input("Enter the column of the battleship: ").upper()
+    return int(row) - 1, letters_to_numbers[column]
+
+
+# checks if it was hit or a miss
+def count_hit_ships(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                count += 1
+    return count
+
+if __name__ == "__main__":
+    create_ships(CONCEALED_BOARD)
+    turns = 15
+    while turns > 0:
+        print('Guess a battleship location')
+        print_board(USER_BOARD)
+        row, column = get_ship_location()
+        if USER_BOARD[row][column] == "-":
+            print("You already guessed that one!")
+        elif CONCEALED_BOARD[row][column] == "X":
+            print("Hit")
+            USER_BOARD[row][column] = "X"
+            turns -= 1
+        else:
+            print("MISS!")
+            USER_BOARD[row][column] = "-"
+            turns -= 1
+        if count_hit_ships(USER_BOARD) == 5:
+            print("You win!")
+            break
+        print("You have " + str(turns) + " turns left!")
+        if turns == 0:
+            print("No more turns!\n")
